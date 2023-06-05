0db46eff5e46bbbfc2b5246dc5476461f95294ab
Fazulhaq Amin <amin.jawan@yahoo.com>
2023-06-05 11:48:36 +0430

    Roles and Permissions in JWT annotation BASED

--- src/main/java/com/example/jwtpractice/demo/ManagementController.java
+++ src/main/java/com/example/jwtpractice/demo/ManagementController.java
@@ -1,23 +1,29 @@
 package com.example.jwtpractice.demo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/management")
+@PreAuthorize("hasAnyRole('ADMIN','MANAGER')")
 public class ManagementController {
     @GetMapping
+    @PreAuthorize("hasAnyAuthority('admin:read','management:read')")
     public String get(){
         return "GET:: management controller";
     }
     @PostMapping
+    @PreAuthorize("hasAnyAuthority('admin:create','management:create')")
     public String post(){
         return "POST:: management controller";
     }
     @PutMapping
+    @PreAuthorize("hasAnyAuthority('admin:update','management:update')")
     public String put(){
         return "PUT:: management controller";
     }
     @DeleteMapping
+    @PreAuthorize("hasAnyAuthority('admin:delete','management:delete')")
     public String delete(){
         return "DELETE:: management controller";
     }
